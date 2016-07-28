d0b315b162f170504eb69f93e10f9247c5363efc
Mr. Liu <18300767078@163.com>
2016-07-28 14:23:05 +0800

    Site updated: 2016-07-28 14:23:04

---
+++ commonweal/search_children.js
@@ -0,0 +1,5 @@
+//var _base = 'http://qzone.qq.com/gy/404/';
+document.write('<script type="text/javascript" src="commonweal/data.js" charset="utf-8"></script>');
+document.write('<script type="text/javascript" src="commonweal/page.js" charset="utf-8"></script>');
+
+/*  |xGv00|bb048611f4d0bb34934fd40353d94edf */
